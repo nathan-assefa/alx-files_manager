57c507007f36a2e4fca506c02008f55085314caa
nathan-assefa <aasamnew@gmail.com>
2023-10-02 00:23:28 -0700

    file manager

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,16 +1,37 @@
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
+const RedisClient = require('../utils/redis');
+const DBClient = require('../utils/db');
 
-class AppController {
-  static getStatus(request, response) {
-    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
-  }
+const AppController = {
+  // Endpoint to check the status of Redis and the database
+  getStatus: async (req, res) => {
+    const redisAlive = RedisClient.isAlive();
+    const dbAlive = await DBClient.isAlive();
 
-  static async getStats(request, response) {
-    const usersNum = await dbClient.nbUsers();
-    const filesNum = await dbClient.nbFiles();
-    response.status(200).json({ users: usersNum, files: filesNum });
-  }
-}
+    const status = {
+      redis: redisAlive,
+      db: dbAlive,
+    };
+
+    return res.status(200).json(status);
+  },
+
+  // Endpoint to get statistics (number of users and files)
+  getStats: async (req, res) => {
+    try {
+      const numUsers = await DBClient.nbUsers();
+      const numFiles = await DBClient.nbFiles();
+
+      const stats = {
+        users: numUsers,
+        files: numFiles,
+      };
+
+      return res.status(200).json(stats);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+};
 
 module.exports = AppController;
